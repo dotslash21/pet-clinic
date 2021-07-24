67741417634bc6be0ecbec7778dba0fc357e3ddb
Arunangshu Biswas <arunangshubsws@gmail.com>
2021-07-24 19:23:30 +0530

    Added owners page and controller, closes #12.

---
+++ pet-clinic-web/src/main/java/xyz/arunangshu/petclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package xyz.arunangshu.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+  @RequestMapping({"", "/", "/index", "/index.html"})
+  public String listOwners() {
+
+    return "owners/index";
+  }
+}
