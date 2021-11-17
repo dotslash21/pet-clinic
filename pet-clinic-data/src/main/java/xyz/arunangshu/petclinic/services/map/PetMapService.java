f8dc45fc95c89f8fb4680c421eb9b965272fa1e4
Arunangshu Biswas <arunangshubsws@gmail.com>
2021-11-17 20:24:09 +0530

    Created Spring Data JPA Vet Specialty Service. Closes #43

--- pet-clinic-data/src/main/java/xyz/arunangshu/petclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/xyz/arunangshu/petclinic/services/map/PetMapService.java
@@ -6,7 +6,7 @@ import xyz.arunangshu.petclinic.model.Pet;
 import xyz.arunangshu.petclinic.services.PetService;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
 
   @Override
   public Set<Pet> findAll() {
