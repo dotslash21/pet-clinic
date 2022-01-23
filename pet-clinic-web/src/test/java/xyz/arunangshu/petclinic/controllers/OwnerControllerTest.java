e52e47b232438f36a2fc05e2a71060f63221b69e
Arunangshu Biswas <arunangshubsws@gmail.com>
2022-01-23 18:54:46 +0530

    Added HTML page for displaying owner record, closes #52

--- pet-clinic-web/src/test/java/xyz/arunangshu/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/xyz/arunangshu/petclinic/controllers/OwnerControllerTest.java
@@ -14,7 +14,7 @@ import xyz.arunangshu.petclinic.services.OwnerService;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -69,5 +69,15 @@ class OwnerControllerTest {
                 .andExpect(view().name("notimplemented"));
 
         verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void showOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 }
